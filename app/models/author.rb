d98543f9a05fff08c2f18ca6d4ae2408480c604c
Varun Battula <varunbattula14@gmail.com>
2016-02-17 03:54:41 -0600

    updated  associtaions ]

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,8 @@
 class Author
   include Mongoid::Document
+
+  has_many :books
+
   field :name, type: String
   field :email, type: String
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book
   include Mongoid::Document
+
+  belongs_to :user
+  belongs_to :author
+  
   field :name, type: String
   field :title, type: String
   field :edition, type: String
